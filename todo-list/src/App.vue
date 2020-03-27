ba06957b684cd420558fb3a79044cc53dc91ad3b
Azat <azatuk2005@mail.ru>
2020-03-27 22:36:09 +0600

    feat: start use Vue-CLI and add general three components

---
+++ todo-list/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <todoList />
+  </div>
+</template>
+
+<script>
+import todoList from '@/components/todoList.vue'
+
+export default {
+  components: {
+    todoList
+  }
+}
+
+</script>
+
+<style>
+
+* {
+  margin: 0;
+  padding: 0;
+  border: 0;
+}
+
+#app {
+  width: 100%;
+  padding: 0.1px;
+  height: 100vh;
+  font-family: 'Open Sans', sans-serif;
+  background-color: #fdcb6e;
+}
+
+</style>
---
+++ todo-list/src/components/todoList.vue
@@ -0,0 +1,47 @@
+<template>
+    <section class="content">
+        <h1 class="content__heading"><i class="fas fa-check"></i>ToDo List</h1>
+        <div class="content__list-container">
+            <div class="todo-list">
+                <todoComponent />
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import todoComponent from '@/components/todoComponent.vue'
+
+export default {
+    components: {
+        todoComponent
+    }
+}
+
+</script>
+
+<style scoped>
+
+.content {
+    margin: auto;
+    margin-top: 10vh;
+    width: 50%;
+    height: auto;
+    font-family: 'Open Sans', sans-serif;
+    text-align: center;
+}
+
+.content__heading {
+    margin: auto;
+    width: 30%;
+    font-size: 50px;
+    font-style: italic;
+    border-bottom: 6px solid #0984e3;
+}
+
+.fa-check {
+    margin-right: 15px;
+    color: #0984e3;
+}
+
+</style>
